a1641526dcc6f4fdb513b6bff4937c8b6c7c706c
Renzo Carara <renzo.carara@libero.it>
2019-11-14 17:49:59 +0100

    inseriti controlli su dati input (età e km negativi o zero)

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
-// rcupero user inputs: lm da percorrere + età utente
+// ----------------- general
+var consistency = true; //se TRUE mi indica che i dati in input sono "buoni"
+
+// ----------------- recupero user inputs: km da percorrere + età utente
 var age = prompt("Inserisci la tua età:", "18");
 var km = prompt("Inserisci i km da percorrere:", "100");
 var sconto = 1.0; // 1=100% cioè nessuno sconto
 
-// realzzare eventuali controlli di consistenza sullo user inputs
+// ----------------- controlli di consistenza sullo user inputs
+if (age < 1 || age > 150) {
+    alert("ATTENZIONE: l'età non è corretta, il sistema non può fornire informazioni. Riprova.");
+    consistency = false;
+}
 
-// verifica applicazione scontistica
+if (km < 1) {
+    alert("ATTENZIONE: i km da percorrere non sono corretti, il sistema non può fornire informazioni. Riprova.");
+    consistency = false;
+}
+
+
+// nascondo il panello con le informazioni di acquisto se non ho dati consistenti
+if (!consistency) {
+    document.getElementById("buy-panel").setAttribute("class", "hide-buy-panel");
+}
+
+// ----------------- verifica applicazione scontistica
 if (age < 18) {
     // 20% di sconto per gli under 18
     sconto = 0.8;
@@ -13,26 +31,24 @@ if (age < 18) {
     // 40% di sconto per gli over 65
     sconto = 0.6;
 }
-// calcolo prezzo in base al kilometrggio e scontistica
-var price = km * 0.21 * sconto;
+// ----------------- calcolo prezzo in base al kilometrggio e scontistica
+// la funzione toFixed(2) mi arrotonda il valore al secondo decimale
+// in modo da avere un valore con al massimo 2 cifre decimali (mitico Luca!)
+var price = (km * 0.21 * sconto).toFixed(2);
 
-console.log("prezzo: ", price);
-
-// popolo gli elementi HTML con inputs ed elaborati
+// ----------------- popolo gli elementi HTML
 document.getElementById("km").innerHTML = km;
 document.getElementById("age").innerHTML = age;
 
-// se non c'e conto visualizzo stringa "nessuno" altrimenti visualizzo lo sconto (un numerico)
+// se non c'e sconto visualizzo la stringa "nessuno" altrimenti visualizzo lo sconto (un numerico)
 if (sconto == 1) {
     // nessuno sconto da applicare per il cliente
-    console.log("nessuno sconto");
+    document.getElementById("discount").innerHTML = "";
     document.getElementById("discount").innerHTML = "nessuno";
 } else {
     // c'è uno sconto da applicare
-    console.log("sconto=", sconto, "  ---   (1 - sconto)=", (1 - sconto));
-    console.log("sconto", Math.round((1 - sconto) * 100));
-    // uso Math.round() per evitare di visualizzare 19.99999 anzichè 20
-    document.getElementById("discount").innerHTML = Math.round((1 - sconto) * 100);
+    // uso Math.round() per evitare di visualizzare 19.999999.. anzichè 20
+    document.getElementById("discount").innerHTML = Math.round((1 - sconto) * 100) + "%";
 }
 
 document.getElementById("price").innerHTML = price;
